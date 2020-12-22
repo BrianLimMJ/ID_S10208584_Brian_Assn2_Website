1b1d7a92cea4635e4e41ffcb9b4b042c6f10f735
BrianLimMJ <S10208584@connect.np.edu.sg>
2020-12-22 14:16:18 +0800

    22 dec

--- servant.js
+++ servant.js
@@ -1,6 +1,6 @@
 let url = 'https://api.atlasacademy.io/export/NA/nice_servant.json';
 
-var i;
+
 
 fetch (url)
     .then(res => res.json())
@@ -12,26 +12,51 @@ fetch (url)
 
 
 document.getElementById("btn").addEventListener("click", function getServant(){
-    let searchName = document.getElementById('Sname');
+    let searchName = document.getElementById('name').value;
 
-    for (i = 0;i<ServantList.length;i++)
+    for (let i = 0;i<ServantList.length;i++)
     {
         fetch (url)
         .then(res => res.json())
         .then(data = ServantList[i]['name'])
-        setTimeout(checking(data,searchName),50); 
+        .then(sGender =ServantList[i]['gender'] )
+        .then(sClass =ServantList[i]['className'])
+        .then(sRarity =ServantList[i]['rarity'] )
+        .then(sNP =ServantList[i]['noblePhantasms'][0]['name'] )
+        .then(sAtkBase =ServantList[i]['atkBase'] )
+        .then(sAtkMax =ServantList[i]['atkMax'] )
+        .then(sHpBase =ServantList[i]['hpBase'] )
+        .then(sHPMax =ServantList[i]['hpMax'] )
+        setTimeout(checking(data,searchName),50);
+        if (data == searchName)
+        {
+            break;
+        }
     }
 
     function checking(data,searchName)
     {
-        if (data == searchName)
+        if (data == searchName) 
         {
-            console.log('Successful' + data);
+            var sName = data;
+            
+            console.log('Servant: '+ sName +
+            '\n'+ 'Gender: '+ sGender + 
+            '\n'+ 'Class: '+ sClass + 
+            '\n'+ 'Rarity: '+ sRarity + 
+            '\n'+ 'Noble Phantasm: '+ sNP + 
+            '\n'+ 'Base Atk: '+ sAtkBase + 
+            '\n'+ 'Max Atk: '+ sAtkMax + 
+            '\n'+ 'Base HP: '+ sHpBase + 
+            '\n'+ 'Max HP: '+ sHPMax );
+            
         }
+        
         else 
         {
             console.log('Failure name not found');
         }
+        
     }
 });
 
